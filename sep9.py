bde014dc84fe4b94c744fe443cae6a7f2b7d299e
Cheeseking1 <104400881+Cheeseking1@users.noreply.github.com>
2024-09-26 20:25:43 -0400

    Add files via upload

---
+++ sep9.py
@@ -0,0 +1,10 @@
+class sky:
+    frog = "square"
+    cheese = "hungrary"
+    cloud = "purple"
+    color = "apple"
+sdfghj = sky
+print(sdfghj.frog)
+print(sdfghj.cheese)
+print(sdfghj.cloud)
+print(sdfghj.color)
