0ddc1dd9272f1d15c10652746ad8dbc658aeb339
Cheeseking1 <104400881+Cheeseking1@users.noreply.github.com>
2024-10-28 20:05:22 -0400

    Add files via upload

---
+++ oct28.py
@@ -0,0 +1,22 @@
+# somethingnotvar = open("sample.txt")
+# print(somethingnotvar)
+# count = 0
+
+
+# for i in somethingnotvar:
+#     print(somethingnotvar.readline())
+
+# somethingnotvar.close()
+
+# new = open("jachin.txt", "w")
+# new.write("hi")
+# new.write("\nhello")
+# new.close()
+# new2 = open("jachin.txt", "a")
+# new2.write("CHEZdaZZZZZZZ")
+# new2.close()
+# new3 = open("jachin.txt", "r")
+# print(new3.readline())
+# print(new3.readline())
+afd = open("nefile.txt", "x")
+
